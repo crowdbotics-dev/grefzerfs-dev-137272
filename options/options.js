fe0a802dfe03b88d37da2965e19961ecae309cfb
Crowdbotics <engineering@crowdbotics.com>
2024-10-11 10:05:44 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "grefzerfs-dev-137272",
+  url: "https://grefzerfs-dev-137272.botics.co",
+  api: "https://grefzerfs-dev-137272.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
